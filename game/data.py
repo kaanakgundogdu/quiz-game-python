621d19e867243917c472984a80c2be0860cd31c0
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-06-06 13:55:23 +0300

    Game files added.

---
+++ game/data.py
@@ -0,0 +1,14 @@
+import requests
+
+question_type = {
+    "amount": 10,
+    "type": "boolean"
+}
+
+resp = requests.get("https://opentdb.com/api.php", params=question_type)
+question_data = {
+
+}
+question_data = resp.json()["results"]
+
+print(question_data)
