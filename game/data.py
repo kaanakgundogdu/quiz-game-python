bb37d1554203bab3e47a57743971748e0e456ff2
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-06-07 15:56:15 +0300

    data, quiz_brain and main python files modified.

--- game/data.py
+++ game/data.py
@@ -1,12 +1,14 @@
 import requests
 
+question_data = {
+
+}
+
 question_type = {
     "amount": 10,
     "type": "boolean"
 }
 
 resp = requests.get("https://opentdb.com/api.php", params=question_type)
-question_data = {
 
-}
 question_data = resp.json()["results"]
